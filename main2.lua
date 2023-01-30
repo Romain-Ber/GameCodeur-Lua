0f51c0d34b430732a7e179d7d389a38dd310fb96
Romain-Ber <romain.bernier@live.fr>
2023-01-30 18:15:39 +0100

    clarity

--- main2.lua
+++ main2.lua
@@ -10,36 +10,36 @@ function love.load()
     screenX = love.graphics.getWidth()
     screenY = love.graphics.getHeight()
     Pad2.x = screenX - Pad2.width
-    Ball.x= screenX/2-Ball.width/2
-    Ball.y= screenY/2-Ball.height/2
+    Ball.x = screenX / 2 - Ball.width / 2
+    Ball.y = screenY / 2 - Ball.height / 2
 end
 
 function love.update()
     if love.keyboard.isDown("s") and Pad1.y < screenY - 80 then
-        Pad1.y = Pad1.y +3
+        Pad1.y = Pad1.y + 3
     end
     if love.keyboard.isDown("w") and Pad1.y > 0 then
-        Pad1.y = Pad1.y -3
+        Pad1.y = Pad1.y - 3
     end
     if love.keyboard.isDown("down") and Pad2.y < screenY - 80 then
-        Pad2.y = Pad2.y +3
+        Pad2.y = Pad2.y + 3
     end
-    if love.keyboard.isDown("up") and Pad2.y>0 then
-        Pad2.y = Pad2.y -3
+    if love.keyboard.isDown("up") and Pad2.y > 0 then
+        Pad2.y = Pad2.y - 3
     end
     Ball.x = Ball.x + Ball.velocityX
     Ball.y = Ball.y + Ball.velocityY
     if Ball.x == screenX-Ball.width then
         Ball.velocityX = - Ball.velocityX
         ScoreP1 = ScoreP1 + 1
-        Ball.x= screenX/2-Ball.width/2
-        Ball.y= screenY/2-Ball.height/2
+        Ball.x = screenX / 2 - Ball.width / 2
+        Ball.y = screenY / 2 - Ball.height / 2
     end
     if Ball.x == 0 then
         Ball.velocityX = - Ball.velocityX
         ScoreP2 = ScoreP2 + 1
-        Ball.x= screenX/2-Ball.width/2 
-        Ball.y= screenY/2-Ball.height/2
+        Ball.x = screenX / 2 - Ball.width / 2
+        Ball.y = screenY / 2 - Ball.height / 2
     end
     if Ball.y == screenY - Ball.height then
         Ball.velocityY = - Ball.velocityY
@@ -48,16 +48,16 @@ function love.update()
         Ball.velocityY = - Ball.velocityY
     end
     if Ball.x == Pad1.width and Ball.y > Pad1.y - Ball.height and Ball.y < Pad1.y + Pad1.height then
-        Ball.velocityX = -Ball.velocityX
+        Ball.velocityX = - Ball.velocityX
     end
     if Ball.x == screenX - Pad2.width - Ball.width and Ball.y > Pad2.y - Ball.height and Ball.y < Pad2.y + Pad2.height then
-        Ball.velocityX = -Ball.velocityX
+        Ball.velocityX = - Ball.velocityX
     end
 end
 
 function love.draw()
-    love.graphics.rectangle("fill",Pad1.x,Pad1.y,Pad1.width,Pad1.height)
-    love.graphics.rectangle("fill",Pad2.x,Pad2.y,Pad2.width,Pad2.height)
-    love.graphics.rectangle("fill",Ball.x,Ball.y,Ball.width,Ball.height)
-    love.graphics.printf(ScoreP1.." - "..ScoreP2, screenX/2,20,200,center)
+    love.graphics.rectangle("fill", Pad1.x, Pad1.y, Pad1.width, Pad1.height)
+    love.graphics.rectangle("fill", Pad2.x, Pad2.y, Pad2.width, Pad2.height)
+    love.graphics.rectangle("fill", Ball.x, Ball.y, Ball.width, Ball.height)
+    love.graphics.printf(ScoreP1.." - "..ScoreP2, screenX / 2, 20, 200, center)
 end
